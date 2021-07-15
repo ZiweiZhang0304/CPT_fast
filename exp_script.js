348ce0696a7c1516bc8880a7c60ec04f70eb22ad
Ziwei Zhang <75584316+ZiweiZhang0304@users.noreply.github.com>
2021-07-15 09:09:14 +0800

    Add files via upload

--- exp_script.js
+++ exp_script.js
@@ -505,8 +505,7 @@ var attention = {
     /*-- If attention <= 80 --*/
     if (at_counter <= 10 ||
         last_infreq.includes('infrequent') ||
-        last_correct.includes(false) ||
-        last_rt.includes(true)  || last_lr==null) //check if last_lr is undefined last_lr == null
+        last_correct.includes(false)) //check if last_lr is undefined last_lr == null
     {lr_node = 0 }//change to 80th trial later}
 
     else if ( last_lr!=null && last_lr.includes(true))  {lr_node = 0 }
